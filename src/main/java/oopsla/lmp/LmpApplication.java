09fb78dfec4a026152256158c39a4266e90e74ee
ySeungJE <dbstmdwp98@naver.com>
2023-01-19 19:04:42 +0900

    test_commit

---
+++ src/main/java/oopsla/lmp/LmpApplication.java
@@ -0,0 +1,13 @@
+package oopsla.lmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmpApplication.class, args);
+	}
+
+}
