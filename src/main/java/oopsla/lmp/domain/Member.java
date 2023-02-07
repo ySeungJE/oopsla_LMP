f51cabfa867324a6d8a6f47686505a72dbb8f6af
KimJuHongg <wnjd0682@gmail.com>
2023-02-07 17:03:55 +0900

    .

--- src/main/java/oopsla/lmp/domain/Member.java
+++ src/main/java/oopsla/lmp/domain/Member.java
@@ -1,11 +1,14 @@
 package oopsla.lmp.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity @Getter @Setter
+@Table(name = "member")
 public class Member {
 
     @Id
